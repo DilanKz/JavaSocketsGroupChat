454d2d851f7bdcf431334ae6c2a1b221c26db8e3
DilanKz <Dilanmallawarachchi555@gmail.com>
2023-05-27 17:31:46 +0530

    Create ChatApplication.java

---
+++ ChatAppPlayTechPvtLtd/ClientApplication/src/lk/PlayTech/ChatApp/ChatApplication.java
@@ -0,0 +1,26 @@
+package lk.PlayTech.ChatApp;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class ChatApplication extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        Parent root= FXMLLoader.load(getClass().getResource("view/ChatForm.fxml"));
+        Scene scene =new Scene(root);
+
+        primaryStage.setScene(scene);
+        primaryStage.setResizable(false);
+        primaryStage.show();
+    }
+}
